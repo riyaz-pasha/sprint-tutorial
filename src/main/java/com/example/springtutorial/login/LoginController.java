53c58d1883e6e72a53bb5d481f40127a7098751f
riyaz <riyazpasha880@gmail.com>
2021-06-08 12:32:18 +0530

    show jsp page... not working

--- src/main/java/com/example/springtutorial/login/LoginController.java
+++ src/main/java/com/example/springtutorial/login/LoginController.java
@@ -1,14 +1,14 @@
 package com.example.springtutorial.login;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginController {
 
     @RequestMapping(path = "/login", method = RequestMethod.GET)
     public String helloWorld() {
-        return "Hello World";
+        return "login";
     }
 }
