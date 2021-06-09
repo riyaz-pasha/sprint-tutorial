ccc790c7939fc9d06c3ea2f7044ff271fa204cbe
riyaz <riyazpasha880@gmail.com>
2021-06-09 15:11:08 +0530

    add JPA and H2 database.. store some user info on the start of the app using commandline runners

---
+++ src/main/java/com/example/springtutorial/user/UserCommandLineRunner.java
@@ -0,0 +1,30 @@
+package com.example.springtutorial.user;
+
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserCommandLineRunner implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private static final Logger log = LoggerFactory.logger(UserCommandLineRunner.class);
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.save(new User("Employee1", "Application Developer Jr"));
+        userRepository.save(new User("Employee2", "Application Developer Sr"));
+        userRepository.save(new User("Employee3", "Business Analyst"));
+        userRepository.save(new User("Employee4", "Project Manager"));
+
+        log.info("--------------------------------");
+        for (User user : userRepository.findAll()) {
+            log.info(user);
+        }
+        log.info("--------------------------------");
+    }
+}
