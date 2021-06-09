84f599aa9d9433242aba9ffc489e4d108bccd1ae
riyaz <riyazpasha880@gmail.com>
2021-06-09 21:56:34 +0530

    Added Unit test for create question

--- src/test/java/com/example/springtutorial/survey/controller/SurveyControllerTest.java
+++ src/test/java/com/example/springtutorial/survey/controller/SurveyControllerTest.java
@@ -9,6 +9,8 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +19,9 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(SurveyController.class)
@@ -46,5 +51,28 @@ public class SurveyControllerTest {
         MvcResult result = mockMvc.perform(requestBuilder).andReturn();
 
         JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+    }
+
+    @Test
+    public void createSurveyQuestion() throws Exception {
+        Question mockQuestion = new Question(
+                "1",
+                "Smallest Number",
+                "1",
+                Arrays.asList("1", "2", "3", "4")
+        );
+        String questionJson = "{\"description\":\"Smallest Number\",\"correctAnswer\":\"1\",\"options\":[\"1\",\"2\",\"3\",\"4\"]}";
+        Mockito.when(surveyService.addQuestion(Mockito.anyString(), Mockito.any(Question.class)))
+                .thenReturn(mockQuestion);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/surveys/Survey1/questions")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(questionJson)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        assertEquals(HttpStatus.CREATED.value(), result.getResponse().getStatus());
+        assertTrue(result.getResponse().getHeader(HttpHeaders.LOCATION).contains("/surveys/Survey1/questions/"));
     }
 }
