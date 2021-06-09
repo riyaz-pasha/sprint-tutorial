18e45c92a2163ac6ae53a96b281a751411006af9
riyaz <riyazpasha880@gmail.com>
2021-06-09 15:48:33 +0530

    add findBuRole Method to the interface which by default implemented by spring JPA

--- src/main/java/com/example/springtutorial/user/UserCommandLineRunner.java
+++ src/main/java/com/example/springtutorial/user/UserCommandLineRunner.java
@@ -16,13 +16,20 @@ public class UserCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        userRepository.save(new User("Employee1", "Application Developer Jr"));
-        userRepository.save(new User("Employee2", "Application Developer Sr"));
+        userRepository.save(new User("Employee1", "Application Developer"));
+        userRepository.save(new User("Employee2", "Application Developer"));
         userRepository.save(new User("Employee3", "Business Analyst"));
         userRepository.save(new User("Employee4", "Project Manager"));
 
         log.info("--------------------------------");
         for (User user : userRepository.findAll()) {
+            log.info(user);
+        }
+        log.info("--------------------------------");
+
+        log.info("--------------------------------");
+        log.info("Application Developers are\n");
+        for (User user : userRepository.findByRole("Application Developer")) {
             log.info(user);
         }
         log.info("--------------------------------");
--- src/main/java/com/example/springtutorial/user/UserRepository.java
+++ src/main/java/com/example/springtutorial/user/UserRepository.java
@@ -2,5 +2,9 @@ package com.example.springtutorial.user;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByRole(String role); // Spring data JPA automatically creates this method findBy{nameOfTheParamToSearch}
 }
